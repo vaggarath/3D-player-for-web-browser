95d32bc176727748b6f02cd37332c4dbe08190d6
vaggarath <jchambon4@gmail.com>
2022-01-20 16:59:59 +0100

    Enhanced controls + annotations + readme

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -8,15 +8,19 @@ class VideoPlayer{
         this.parent = _divParent;
         this.player = _divPlayer;
         this.model = _model
-        this.poi = _poi
+        this.poi = _poi ? _poi : null
+        this.player = null
+        this.playerCtrl = null
+        this.sidebar = null
+        this.show = false;
     }
 
     buildPlayer(){
         const player = document.createElement('model-viewer')
-        player.id = this.player;
+        player.id = "player";
         player.setAttribute('src', this.model)
         player.setAttribute('reveal', "interaction")
-        player.setAttribute('environment-image', "../modeles/pillars_1k.hdr")
+        player.setAttribute('skybox-image', "modeles/hall_of_finfish_4k.hdr")
         player.setAttribute('shadow-intensity', "0")
         player.setAttribute('exposure', "1")
         player.setAttribute('ar-modes', "webxr scene-viewer quick-look")
@@ -35,14 +39,22 @@ class VideoPlayer{
         player.append(poster)
         player.append(loadButton)
         document.getElementById(this.parent).append(player)
-
+        this.player = player
         return player;
     }
-
+    /**
+     * 
+     * @builds the play/pause option 
+     */
     buildControls(){
-        const player = document.getElementById(this.player)
-        // const parts = player.model.materials;
-        // console.log(parts)
+        const player = this.player //document.getElementById(this.player)
+        const defaultAnimation = player.availableAnimations && player.availableAnimations[0] ? player.availableAnimations[0] : false;
+
+        // console.log(player.availableAnimations[0])
+
+        if(defaultAnimation){
+            player.setAttribute("animation-name", defaultAnimation)
+        }
 
         const playerParent = document.createElement('div')
         playerParent.id ="myProgress"
@@ -100,7 +112,7 @@ class VideoPlayer{
                     i = 0;
                     elem.style.width = "0%"
                     if(playBtn.classList.contains('fa-pause-circle')){
-                        console.log('oui')
+                        // console.log('oui')
                         playBtn.classList.remove('fa-pause-circle')
                         playBtn.classList.add('fa-play-circle')
                     }else{
@@ -115,12 +127,17 @@ class VideoPlayer{
               }
             }
         }
+
+        this.playerCtrl = playerParent
+        return playerParent;
     }
 
-    buildHiders(){
+    buildSidebar(){
         const parent = document.getElementById(this.parent);
-        const player = document.getElementById(this.player)
+        const player = this.player
         const parts = player.model.materials;
+        const defaultAnimation = player.availableAnimations && player.availableAnimations[0] ? player.availableAnimations[0] : false;
+        const animationList = player.availableAnimations && player.availableAnimations.length > 1 ? player.availableAnimations : []
 
         const sideBar = document.createElement('div')
         sideBar.id ="champ-info_troisd"
@@ -134,7 +151,19 @@ class VideoPlayer{
         hideTitle.classList.add('mt-1')
         hideTitle.innerText = "Show / Hide elements"
 
+        const hideSidebar = document.createElement('button')
+        hideSidebar.onclick = (e) =>{
+            e.preventDefault()
+            this.sidebar.remove()
+            this.showSideBar();
+            // document.getElementById(this.parent).append(showSidebar)
+        }
+        hideSidebar.id = "hideSidebar"
+        hideSidebar.innerHTML='<i class="fas fa-window-close"></i>'
+        hideSidebar.setAttribute('style', 'position:absolute;right:1rem;')
         
+
+        sideBar.appendChild(hideSidebar)
 
         sideBar.append(hideTitle)
         sideBar.append(hr)
@@ -168,6 +197,16 @@ class VideoPlayer{
             } 
             divCtrl.append(showBtn)
 
+            const colorBtn = document.createElement('i')
+            colorBtn.classList.add('fas')
+            colorBtn.classList.add('fa-paint-roller')
+            colorBtn.setAttribute('data-model', id)
+            colorBtn.onclick = (e)=>{
+                const material = parts[parseInt(e.target.getAttribute("data-model"))]
+                material != null ? material.pbrMetallicRoughness.setBaseColorFactor([Math.random(), Math.random(), Math.random()]) : ""
+            }
+            divCtrl.append(colorBtn)
+
             const partName = document.createElement('h5')
             partName.innerText = part.name
             divCtrl.append(partName)
@@ -175,7 +214,144 @@ class VideoPlayer{
             sideBar.append(divCtrl)
             
         })
+
         parent.append(sideBar)
+        this.sidebar = sideBar
+
+        if(animationList.length > 0){
+            this.buildAnimationSelector(animationList)
+        }
+
+        if(this.poi){
+            this.hotSpotBtn();
+        }
+
+        return this.sidebar;
+    }
+
+    buildAnimationSelector=(options)=>{
+        // console.log(this.sidebar)
+        const modelViewer = this.player
+        const side = document.getElementById(this.sidebar.id)
+
+        const title = document.createElement('h5')
+        title.classList.add('mt-5')
+        title.innerText = `This model possesses ${options.length} animations`
+
+        const titleAfterHr = document.createElement('hr')
+
+        const select = document.createElement('select')
+        options.map((opt, id)=>{
+            const option = document.createElement('option')
+            option.value = opt
+            option.id = id
+            option.label = opt
+            select.append(option)
+        })
+
+        select.onchange = (e) =>{
+            modelViewer.pause()
+            modelViewer.animationName = e.target.value
+        }
+
+        side.append(title)
+        side.append(titleAfterHr)
+        side.append(select)
+        // console.log(this)
+    }
+
+    showSideBar(){
+        console.log("blaah")
+        const showSidebarBtn = document.createElement('button')
+            showSidebarBtn.onclick=(e)=>{
+                e.preventDefault();
+                this.buildSidebar();
+                e.target.remove();
+            }
+            showSidebarBtn.id = "showSideBar"
+            showSidebarBtn.innerHTML = '<i class="fas fa-cogs"></i>'
+            // showSidebarBtn.setAttribute('style', 'position:absolute;top:1rem;right:1rem;')
+            
+        
+        document.getElementById(this.parent).append(showSidebarBtn)
+    }
+
+    hotSpotBtn(){
+        console.log(this.sidebar)
+        const modelViewer = this.player
+        const json = this.poi
+        const sidebar = this.sidebar
+
+        const hr = document.createElement('hr')
+        const title = document.createElement('h5')
+        title.classList.add('mt-5')
+        title.innerText = "Show points of interests"
+        const poiBtn = document.createElement('button')
+        poiBtn.id ="poi"
+        poiBtn.innerText ="show"
+        poiBtn.onclick = (e) =>{
+            e.preventDefault();
+            if(!this.show){
+                fetch(json)
+                .then(res=>res.json())
+                .then((data)=>{
+                    const hotspots = data.hotspots
+                    for(let i=0; i<hotspots.length; i++){
+                        // console.log(hotspots[i])
+                        const spot = document.createElement('button')
+                        spot.innerText = hotspots[i]["text"]
+                        spot.classList.add('view-button')
+                        spot.setAttribute('slot', hotspots[i]["slot"])
+                        spot.setAttribute('data-position', hotspots[i]["data-position"])
+                        spot.setAttribute('data-normal', hotspots[i]["data-normal"])
+                        spot.setAttribute('data-visibility-attribute', hotspots[i]["data-visibility-attribute"])
+                        // spot.setAttribute('slot', hotspots[i]["slot"])
+                        spot.onclick = (e)=>{
+                            e.preventDefault();
+                            modelViewer.cameraTarget = hotspots[i]["data-position"];
+                            modelViewer.cameraOrbit = hotspots[i]["data-orbit"];
+                        }
+    
+                        console.log(spot)
+                        modelViewer.append(spot)
+                    }
+                })
+                poiBtn.innerText = "hide"
+                this.show = true;
+            }else{
+                const elements = document.getElementsByClassName('view-button');
+                while(elements.length > 0){
+                elements[0].parentNode.removeChild(elements[0]);
+                }
+                poiBtn.innerText = "show"
+                this.show = false;
+            }
+        }
+
+        sidebar.append(title)
+        sidebar.append(hr)
+        sidebar.append(poiBtn)
+    }
+
+    addTextureAndColor(){
+        const modelViewerTexture = this.player;
+        const materials = modelViewerTexture.model.materials;
+
+        if(materials.length > 0){
+            materials.map((material)=>{
+                material.pbrMetallicRoughness.setBaseColorFactor([Math.random(), Math.random(), Math.random()])
+                material.pbrMetallicRoughness.setRoughnessFactor(1);
+            })
+        }
     }
 
 }
+
+/***
+ * <button class="Hotspot" 
+ * slot="hotspot-1" 
+ * data-position="-4.2187474980126485m -0.7969916084390367m -6.023500668861005m" data-normal="0m 1m -2.2204460492503128e-16m" data-visibility-attribute="visible">
+ 
+<button class="Hotspot" slot="hotspot-2" data-position="-9.492227459642127m 0.8358180974986169m 1m" data-normal="0m 0m 1m" data-visibility-attribute="visible">
+
+*/
--- src/main.js
+++ src/main.js
@@ -12,10 +12,156 @@ let player = new VideoPlayer("crane", "lazy-load", "modeles/5582b6d0-ad24-46e3-a
  */
 const buildedPlayer = player.buildPlayer();
 
-buildedPlayer.addEventListener('load', ()=>{
+const buildControls = () =>{
     /**
-     * once the player id loaded : builds its  controls and displays them
+     * once the player is loaded : builds its  controls and displays them
+     * if you only want to display the 3D model, then do not proceed with this step
      */
-    player.buildControls()
-    player.buildHiders()
-})
+    buildedPlayer.addEventListener('load', ()=>{
+        player.buildControls();
+        player.buildSidebar();
+
+        player.addTextureAndColor(); //only call this function if you use a blank model and want its parts colorized. The color is randomly generated. it also setRoughnessFactor to 1
+
+        const modelViewer = document.querySelector("#player");
+        const tapDistance = 2;
+        let panning = false;
+        let panX, panY;
+        let startX, startY;
+        let lastX, lastY;
+        let metersPerPixel;
+
+        const startPan = () => {
+            const orbit = modelViewer.getCameraOrbit();
+            const {theta, phi, radius} = orbit;
+            const psi = theta - modelViewer.turntableRotation;
+            metersPerPixel = 0.75 * radius / modelViewer.getBoundingClientRect().height;
+            panX = [-Math.cos(psi), 0, Math.sin(psi)];
+            panY = [
+            -Math.cos(phi) * Math.sin(psi),
+            Math.sin(phi),
+            -Math.cos(phi) * Math.cos(psi)
+            ];
+            modelViewer.interactionPrompt = 'none';
+        };
+
+        const stopPan = (event) => {
+            panning = false;
+        }
+
+        const movePan = (thisX, thisY) => {
+            const dx = (thisX - lastX) * metersPerPixel;
+            const dy = (thisY - lastY) * metersPerPixel;
+            lastX = thisX;
+            lastY = thisY;
+
+            const target = modelViewer.getCameraTarget();
+            target.x += dx * panX[0] + dy * panY[0];
+            target.y += dx * panX[1] + dy * panY[1];
+            target.z += dx * panX[2] + dy * panY[2];
+            modelViewer.cameraTarget = `${target.x}m ${target.y}m ${target.z}m`;
+
+            // This pauses turntable rotation
+            modelViewer.dispatchEvent(new CustomEvent(
+                'camera-change', {detail: {source: 'user-interaction'}}));
+        };
+
+        const recenter = (pointer) => {
+            panning = false;
+            if (Math.abs(pointer.clientX - startX) > tapDistance ||
+                Math.abs(pointer.clientY - startY) > tapDistance)
+            return;
+            const hit = modelViewer.positionAndNormalFromPoint(pointer.clientX, pointer.clientY);
+            if(hit != null) {
+            modelViewer.cameraTarget = hit.position.toString();
+            }
+            else {
+            modelViewer.cameraTarget = 'auto auto auto';
+            modelViewer.cameraOrbit = 'auto auto auto';
+            }
+        };
+
+        modelViewer.addEventListener('mousedown', (event) => {
+            startX = event.clientX;
+            startY = event.clientY;
+            panning = event.button === 2 || event.ctrlKey || event.metaKey ||
+                event.shiftKey;
+            if (!panning)
+            return;
+
+            lastX = startX;
+            lastY = startY;
+            startPan();
+            event.stopPropagation();
+        }, true);
+
+        modelViewer.addEventListener('touchstart', (event) => {
+            const {targetTouches, touches} = event;
+            startX = targetTouches[0].clientX;
+            startY = targetTouches[0].clientY;
+            panning = targetTouches.length === 2 && targetTouches.length === touches.length;
+            if (!panning)
+            return;
+
+            lastX = 0.5 * (targetTouches[0].clientX + targetTouches[1].clientX);
+            lastY = 0.5 * (targetTouches[0].clientY + targetTouches[1].clientY);
+            startPan();
+        }, true);
+
+        self.addEventListener('mousemove', (event) => {
+            if (!panning)
+            return;
+
+            movePan(event.clientX, event.clientY);
+            event.stopPropagation();
+        }, true);
+
+        modelViewer.addEventListener('touchmove', (event) => {
+            if (!panning || event.targetTouches.length !== 2)
+            return;
+
+            const {targetTouches} = event;
+            const thisX = 0.5 * (targetTouches[0].clientX + targetTouches[1].clientX);
+            const thisY = 0.5 * (targetTouches[0].clientY + targetTouches[1].clientY);
+            movePan(thisX, thisY);
+        }, true);
+
+        let lastMousedown = {time: new Date().getTime()};
+        let lastTouchstart = {time: new Date().getTime()};
+        function doubletap(timer) {
+            var now = new Date().getTime();
+            var timesince = now - timer.time;
+            timer.time = new Date().getTime();
+            if((timesince < 400) && (timesince > 0))
+            return true;
+            else
+            return false;
+        }
+
+        self.addEventListener('mouseup', (event) => {
+            stopPan(event);
+        }, true);
+
+        modelViewer.addEventListener('oncontextmenu', (event) => {
+            return false;
+        });
+
+        self.addEventListener('mousedown', (event) => {
+            if(doubletap(lastMousedown))
+                recenter(event);
+        }, true);
+        
+        modelViewer.addEventListener('touchstart', (event) => {
+            if (doubletap(lastTouchstart) && event.targetTouches.length === 0) {
+            recenter(event.changedTouches[0]);
+
+            if (event.cancelable) {
+                event.preventDefault();
+            }
+            }
+        }, true);
+    })
+}
+
+buildControls();
+
